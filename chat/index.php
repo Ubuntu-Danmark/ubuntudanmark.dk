c414ccf9b458d3e0c096e172cb6d64304283435e
Anders Jenbo <ajenbo@fcomputer.dk>
2011-09-13 21:55:08 +0200

    Redirect to new webchat

---
+++ chat/index.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://webchat.freenode.net/?nick=Ubuntubruger.&channels=ubuntu-dk&uio=MT1mYWxzZSYzPWZhbHNlJjk9dHJ1ZSYxMD10cnVl32");
+?> 
