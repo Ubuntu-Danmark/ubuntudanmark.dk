441760461079f103ecbaf9148c8930f39b64bba0
Anders Jenbo <anders@jenbo.dk>
2015-11-16 04:22:00 +0100

    Use get_avatar_after tricker from phpBB 3.1.6

--- forum/ext/ajenbo/defaultgravatar/event/main_listener.php
+++ forum/ext/ajenbo/defaultgravatar/event/main_listener.php
@@ -22,13 +22,10 @@ class main_listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header'					=> 'navbar_replace_avatar',
-			'core.viewtopic_post_rowset_data'	=> 'viewtopic_replace_avatar',
-			'core.ucp_pm_view_messsage'			=> 'ucp_message_replace_avatar',
+			'core.get_avatar_after' => 'getGravatar',
 		);
 	}
 
-	private $user = null;
 	private $size = 100;
 	private $default_avatar = '';
 
@@ -43,43 +40,8 @@ class main_listener implements EventSubscriberInterface
 		$template_path = $template->get_user_style();
 		$template_path = $template_path[0];
 
-		$this->user = $user;
 		$this->size = (int) $config['default_gravatar_size'] ?: 80;
 		$this->default_avatar = $root_path . 'styles/' . $template_path . '/theme/images/no_avatar.gif';
-	}
-
-	/**
-	* Overwrites the avatar settings in the navbar
-	*
-	* @param mixed $event
-	*/
-	public function navbar_replace_avatar($event_OBJ)
-	{
-		if (!$this->user->data['user_avatar'])
-		{
-			$this->user->data['user_avatar'] = $this->get_gravatar_url($this->user->data['user_email']);
-			$this->user->data['user_avatar_type'] = 'avatar.driver.remote';
-			$this->user->data['user_avatar_width'] = $this->size;
-			$this->user->data['user_avatar_height'] = $this->size;
-    	}
-	}
-
-	/**
-	* Overwrites the avatar settings of the poster
-	*
-	* @param mixed $event
-	*/
-	public function viewtopic_replace_avatar($event_OBJ)
-	{
-		if (!$event_OBJ['row']['user_avatar'])
-		{
-			$row = $event_OBJ['row'];
-			$row['user_avatar'] = $this->get_gravatar_url($row['user_email']);
-			$row['user_avatar_type'] = 'avatar.driver.remote';
-			$row['user_avatar_width'] = $this->size;
-			$row['user_avatar_height'] = $this->size;
-			$event_OBJ['row'] = $row;
-		}
 	}
 
 	/**
@@ -87,34 +49,24 @@ class main_listener implements EventSubscriberInterface
 	*
 	* @param mixed $event
 	*/
-	public function ucp_message_replace_avatar($event_OBJ)
+	public function getGravatar($param)
 	{
-		if (!$event_OBJ['msg_data']['AUTHOR_AVATAR'])
-		{
-			$msg_data = $event_OBJ['msg_data'];
-			$msg_data['AUTHOR_AVATAR'] = '<img src="'
-				. htmlspecialchars($this->get_gravatar_url($event_OBJ['message_row']['user_email']))
+		if (empty($param['html']) && !empty($param['row']['email'])) {
+			$email = $param['row']['email'];
+
+			$url = $this->default_avatar;
+			if (trim($email)) {
+				//TODO configurable default image
+				$url = '//secure.gravatar.com/avatar/'
+					. md5(strtolower(trim($email)))
+					. '?d=mm&s=' . $this->size;
+			}
+
+			$html = '<img src="' . htmlspecialchars($url)
 				. '" width="' . $this->size
 				. '" height="' . $this->size . '" />';
-			$event_OBJ['msg_data'] = $msg_data;
-		}
-	}
 
-	/**
-	* Build gravatar URL for output on page
-	*
-	* @param string  $email User email
-	* @return string Gravatar URL
-	*/
-	protected function get_gravatar_url($email)
-	{
-		if (!trim($email)) {
-			return $this->default_avatar;
+			$param['html'] = $html;
 		}
-
-		//TODO configurable default image
-		return '//secure.gravatar.com/avatar/'
-			. md5(strtolower(trim($email)))
-			. '?d=mm&s=' . $this->size;
 	}
 }
