3e9b1d5a79b57329115f6bedc48315efe6e6f41b
Anders <anders@jenbo.dk>
2015-12-03 17:26:10 +0100

    Clean up the menu code

--- forum/styles/light-phpbb-theme/wp_menu.php
+++ forum/styles/light-phpbb-theme/wp_menu.php
@@ -2,14 +2,24 @@
 define('WP_USE_THEMES', false);
 require('../../../' . 'wp-load.php');
 
-//ob_start();
+ob_start();
 thematic_header();
-exit;
 $primary_header_menu = ob_get_clean();
 $primary_header_menu = preg_replace(
-    '#(.+)class="menu-item (.+?)href="/forum/"#s',
-    '\1class="menu-item current-menu-item \2href="/forum/"',
-    $primary_header_menu
+	'#(.+)class="menu-item (.+?)href="/forum/"#s',
+	'\1class="menu-item current-menu-item \2href="/forum/"',
+	$primary_header_menu
 );
 preg_match('#.+href="/forum/".+?/a>#s', $primary_header_menu, $match);
-echo $match[0];
+
+header('Content-Type: application/json');
+echo json_encode(
+	[
+		$match[0],
+		mb_substr($primary_header_menu, mb_strlen($match[0])),
+	]
+);
+
+if (function_exists('fastcgi_finish_request')) {
+	fastcgi_finish_request();
+}
